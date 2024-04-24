37a1473832277f22dfe9fdbd72d03ca0c9095ed4
netotest <stepanmokan@ya.ru>
2024-04-24 15:38:51 +0300

    FirstCommit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/lru0juhwais9i1yk?svg=true)](https://ci.appveyor.com/project/StefanRomanescu/aqa-2-postmanecho)
---
+++ src/test/java/MobileBankApiTestV1.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnAccounts() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
